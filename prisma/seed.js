8a3cd5356bec897e76c44f88706080cd934de2ea
Alexander Prechtel <aprechtel0043@gmail.com>
2026-04-21 12:58:56 -0400

    Changing Seed Script

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,18 +6,17 @@ const isDev = process.env.NODE_ENV === 'development';
 try {
   if (isDev) {
     await prisma.$queryRaw`TRUNCATE books, authors, users, reviews RESTART IDENTITY CASCADE;`;
-    console.log('[DEVELOPMENT] Database truncated');
+    console.log('[SEED] [DEVELOPMENT] Database truncated');
   }
-
-  const usersData = [
-    { email: 'example@example.com', password: 'examplePassword' },
-    { email: 'johnDoe@email.com', password: 'johndoe1234' },
-    { email: 'janedoe@email.com', password: 'janedoe1234' },
-    { email: 'admin@admin.com', password: 'adminpassword', role: 'ADMIN' },
-  ];
-  const users = [];
   const userCount = await prisma.user.count();
   if (userCount === 0) {
+    const usersData = [
+      { email: 'example@example.com', password: 'examplePassword' },
+      { email: 'johnDoe@email.com', password: 'johndoe1234' },
+      { email: 'janedoe@email.com', password: 'janedoe1234' },
+      { email: 'admin@admin.com', password: 'adminpassword', role: 'ADMIN' },
+    ];
+    const users = [];
     for (const userData of usersData) {
       const hashedPassword = await bcrypt.hash(userData.password, 10);
       const user = await prisma.user.create({
@@ -35,95 +34,110 @@ try {
   } else {
     console.log('[SEED] Users found in database skipping');
   }
+  const authorCount = await prisma.author.count();
+  if (authorCount === 0) {
+    const authorData = [
+      { name: 'Alex Prechtel' },
+      { name: 'John Doe' },
+      { name: 'Jane Doe' },
+      { name: 'Bob Smith' },
+    ];
+    await prisma.author.createMany({ data: authorData });
+    console.log('[SEED] Authors Created.');
+  } else {
+    console.log('[SEED] Authors found in database skipping');
+  }
 
-  const authorData = [
-    { name: 'Alex Prechtel' },
-    { name: 'John Doe' },
-    { name: 'Jane Doe' },
-    { name: 'Bob Smith' },
-  ];
-  await prisma.author.createMany({ data: authorData });
-  console.log('[SEED] Authors Created.');
-  const bookData = [
-    {
-      name: 'The Very Hungry Caterpillar',
-      price: 9.99,
-      published: new Date(new Date('2026-03-29').toISOString()).toISOString(),
-      authorId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      name: 'Booky Book',
-      price: 19.99,
-      published: new Date('2026-03-29').toISOString(),
-      authorId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      name: 'Booky Book 2',
-      price: 19.99,
-      published: new Date('2026-03-29').toISOString(),
-      authorId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      name: 'Book the Book',
-      price: 19.99,
-      published: new Date('2026-03-29').toISOString(),
-      authorId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      name: 'Book the Book 2',
-      price: 19.99,
-      published: new Date('2026-03-29').toISOString(),
-      authorId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      name: 'Book the Book 3',
-      price: 19.99,
-      published: new Date('2026-03-29').toISOString(),
-      authorId: Math.floor(Math.random() * 4) + 1,
-    },
-  ];
-  await prisma.book.createMany({ data: bookData });
-  console.log('[SEED] Books Created.');
+  const bookCount = await prisma.book.count();
+  if (bookCount === 0) {
+    const bookData = [
+      {
+        name: 'The Very Hungry Caterpillar',
+        price: 9.99,
+        published: new Date('2026-03-29').toISOString(),
+        authorId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        name: 'Booky Book',
+        price: 19.99,
+        published: new Date('2026-03-29').toISOString(),
+        authorId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        name: 'Booky Book 2',
+        price: 19.99,
+        published: new Date('2026-03-29').toISOString(),
+        authorId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        name: 'Book the Book',
+        price: 19.99,
+        published: new Date('2026-03-29').toISOString(),
+        authorId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        name: 'Book the Book 2',
+        price: 19.99,
+        published: new Date('2026-03-29').toISOString(),
+        authorId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        name: 'Book the Book 3',
+        price: 19.99,
+        published: new Date('2026-03-29').toISOString(),
+        authorId: Math.floor(Math.random() * 4) + 1,
+      },
+    ];
+    await prisma.book.createMany({ data: bookData });
+    console.log('[SEED] Books Created.');
+  } else {
+    console.log('[SEED] Books found in database skipping');
+  }
 
-  const reviewData = [
-    {
-      rating: Math.floor(Math.random() * 5) + 1,
-      content:
-        'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
-      userId: Math.floor(Math.random() * 4) + 1,
-      bookId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      rating: Math.floor(Math.random() * 5) + 1,
-      content:
-        'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
-      userId: Math.floor(Math.random() * 4) + 1,
-      bookId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      rating: Math.floor(Math.random() * 5) + 1,
-      content:
-        'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
-      userId: Math.floor(Math.random() * 4) + 1,
-      bookId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      rating: Math.floor(Math.random() * 5) + 1,
-      content:
-        'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
-      userId: Math.floor(Math.random() * 4) + 1,
-      bookId: Math.floor(Math.random() * 4) + 1,
-    },
-    {
-      rating: Math.floor(Math.random() * 5) + 1,
-      content:
-        'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
-      userId: Math.floor(Math.random() * 4) + 1,
-      bookId: Math.floor(Math.random() * 4) + 1,
-    },
-  ];
-  await prisma.review.createMany({ data: reviewData });
-  console.log('[SEED] Reviews Created.');
+  const reviewCount = await prisma.review.count();
+  if (reviewCount === 0) {
+    const reviewData = [
+      {
+        rating: Math.floor(Math.random() * 5) + 1,
+        content:
+          'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
+        userId: Math.floor(Math.random() * 4) + 1,
+        bookId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        rating: Math.floor(Math.random() * 5) + 1,
+        content:
+          'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
+        userId: Math.floor(Math.random() * 4) + 1,
+        bookId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        rating: Math.floor(Math.random() * 5) + 1,
+        content:
+          'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
+        userId: Math.floor(Math.random() * 4) + 1,
+        bookId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        rating: Math.floor(Math.random() * 5) + 1,
+        content:
+          'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
+        userId: Math.floor(Math.random() * 4) + 1,
+        bookId: Math.floor(Math.random() * 4) + 1,
+      },
+      {
+        rating: Math.floor(Math.random() * 5) + 1,
+        content:
+          'deserunt eiusmod reprehenderit voluptate velit ad sunt labore dolor tempor',
+        userId: Math.floor(Math.random() * 4) + 1,
+        bookId: Math.floor(Math.random() * 4) + 1,
+      },
+    ];
+    await prisma.review.createMany({ data: reviewData });
+    console.log('[SEED] Reviews Created.');
+  } else {
+    console.log('[SEED] Reviews found in database skipping');
+  }
 } catch (e) {
   console.error('seed failed: ', e);
 }
